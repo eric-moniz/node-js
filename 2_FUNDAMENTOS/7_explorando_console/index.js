09bfbcd41c85505b87164f3dd517446ac1ea86ca
eric-moniz <ericmoniz@outlook.com.br>
2024-08-11 10:10:05 -0300

    secao 2 - explorando o console

---
+++ 2_FUNDAMENTOS/7_explorando_console/index.js
@@ -0,0 +1,18 @@
+// mais de uma variavel
+const x = 10;
+const y = 'Marcos';
+const z = [1, 2];
+
+console.log(x, y, z);
+
+// contagem de impressoes
+console.count(`O valor de x é: ${x}, contagem`);
+console.count(`O valor de x é: ${x}, contagem`);
+console.count(`O valor de x é: ${x}, contagem`);
+console.count(`O valor de x é: ${x}, contagem`);
+
+// variavel entre string
+console.log('O nome é %s, ele é programador', y);
+
+// limpar o console
+setTimeout(() => console.clear(), 2000);
