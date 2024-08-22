d1395acd9f495e68f01efa703f86f1a3c9ab003b
eric-moniz <ericmoniz@outlook.com.br>
2024-08-22 20:56:23 -0300

    secao 5 - Accounts - finalizando criação da conta

--- 5_ACCOUNTS_APP/index.js
+++ 5_ACCOUNTS_APP/index.js
@@ -31,4 +31,39 @@ function operation() {
 function createAccount() {
     console.log(chalk.bgGreen.black.bold('Parabéns por escolher nosso banco!'));
     console.log(chalk.green('Defina as opções da sua conta a seguir'));
+
+    buildAccount();
+}
+
+function buildAccount() {
+    inquirer
+        .prompt([
+            {
+                name: 'accountName',
+                message: 'Digite um nome para sua conta',
+            },
+        ])
+        .then(answear => {
+            const accountName = answear['accountName'];
+
+            console.info(accountName);
+
+            if (!fs.existsSync('accounts')) {
+                fs.mkdirSync('accounts');
+            }
+
+            if (fs.existsSync(`accounts/${accountName}.json`)) {
+                console.log(chalk.bgRed.black.bold('Esta conta já existe, escolha outro nome!'));
+                buildAccount();
+                return;
+            }
+
+            fs.writeFileSync(`accounts/${accountName}.json`, '{ "balance": 0 }', function err() {
+                console.log(chalk.bgRed.white.bold(err));
+            });
+
+            console.log(chalk.green('Parabéns, sua conta foi criada com sucesso!'));
+            operation();
+        })
+        .catch(err => console.log(chalk.bgRed.white.bold(err)));
 }
