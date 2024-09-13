76f1116ad80956623f4374c4d031b3ce24eb3013
eric-moniz <ericmoniz@outlook.com.br>
2024-09-13 20:45:53 -0300

    secao 8 - Node e MySQL - resgatando dados

---
+++ 8_NODE_MYSQL/3_resgatando_dados/index.js
@@ -0,0 +1,71 @@
+const express = require('express');
+const exphbs = require('express-handlebars');
+const mysql = require('mysql');
+
+const app = express();
+
+// pegar os dados do body
+app.use(
+    express.urlencoded({
+        extended: true,
+    })
+);
+
+app.use(express.json());
+
+app.engine('handlebars', exphbs.engine());
+app.set('view engine', 'handlebars');
+
+app.use(express.static('public'));
+
+app.get('/', (req, res) => {
+    res.render('home');
+});
+
+app.post('/books/insertbook', (req, res) => {
+    const title = req.body.title;
+    const pageqty = req.body.pageqty;
+
+    const sql = `INSERT INTO books (title, pageqty) VALUES ('${title}','${pageqty}')`;
+
+    conn.query(sql, err => {
+        if (err) {
+            console.log(err);
+            return;
+        }
+
+        res.redirect('/books');
+    });
+});
+
+app.get('/books', (req, res) => {
+    const sql = 'SELECT * FROM books';
+
+    conn.query(sql, (err, data) => {
+        if (err) {
+            console.log(err);
+            return;
+        }
+
+        const books = data;
+
+        res.render('books', { books });
+    });
+});
+
+const conn = mysql.createConnection({
+    host: 'localhost',
+    user: 'root',
+    password: '089110Eric',
+    database: 'nodemysql1',
+});
+
+conn.connect(err => {
+    if (err) {
+        console.log(err);
+        return;
+    }
+
+    console.log('Connected to DB');
+    app.listen(3000, () => console.log('Server running'));
+});
