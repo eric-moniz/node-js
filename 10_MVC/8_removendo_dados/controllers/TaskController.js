143ded6a91fc61ed7b0b8a2ac3b021c84b07e670
eric-moniz <ericmoniz@outlook.com.br>
2024-09-28 13:44:51 -0300

    secao 10 - MVC - Removendo dados

---
+++ 10_MVC/8_removendo_dados/controllers/TaskController.js
@@ -0,0 +1,36 @@
+const Task = require('../models/Task');
+
+module.exports = class TaskController {
+    static createTask(req, res) {
+        res.render('tasks/create');
+    }
+
+    static async createTaskSave(req, res) {
+        const { title, description } = req.body;
+
+        const task = {
+            title,
+            description,
+            done: false,
+        };
+
+        await Task.create(task);
+
+        res.redirect('/tasks');
+    }
+
+    static async removeTask(req, res) {
+        const id = req.body.id;
+        console.log('************:', id);
+
+        await Task.destroy({ where: { id } });
+
+        res.redirect('/tasks');
+    }
+
+    static async showTasks(req, res) {
+        const tasks = await Task.findAll({ raw: true });
+
+        res.render('tasks/all', { tasks });
+    }
+};
