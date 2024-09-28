3a3a0c60b0d5709e848aac09928a91282a625011
eric-moniz <ericmoniz@outlook.com.br>
2024-09-28 15:01:12 -0300

    secao 10 - MVC - Iniciando a edição

---
+++ 10_MVC/9_edicao_dados/controllers/TaskController.js
@@ -0,0 +1,45 @@
+const { where } = require('sequelize');
+const Task = require('../models/Task');
+
+module.exports = class TaskController {
+    static createTask(req, res) {
+        res.render('tasks/create');
+    }
+
+    static async createTaskSave(req, res) {
+        const { title, description } = req.body;
+
+        const task = {
+            title,
+            description,
+            done: false,
+        };
+
+        await Task.create(task);
+
+        res.redirect('/tasks');
+    }
+
+    static async removeTask(req, res) {
+        const id = req.body.id;
+        console.log('************:', id);
+
+        await Task.destroy({ where: { id } });
+
+        res.redirect('/tasks');
+    }
+
+    static async updateTask(req, res) {
+        const id = req.params.id;
+
+        const task = await Task.findOne({ where: { id }, raw: true });
+
+        res.render('tasks/edit', { task });
+    }
+
+    static async showTasks(req, res) {
+        const tasks = await Task.findAll({ raw: true });
+
+        res.render('tasks/all', { tasks });
+    }
+};
